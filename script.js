74d24ae0a76782672a7604ea8244a4d4e778bfbe
HomerPapryk <paprykhomer@gmail.com>
2024-08-30 12:36:39 +0200

    Poprawka1

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 function sentenceToUpperCase(sentence) {
   let words = sentence.split(" ");
 
+  // Jeśli w zdaniu jest tylko jedno słowo, zwróć je jako wielkie litery
+  if (words.length === 1) {
+    return words[0].toUpperCase();
+  }
+
   let transformedWords = words.map((word, index) => {
     if (index % 2 !== 0) {
       return word.toUpperCase();
@@ -12,6 +17,6 @@ function sentenceToUpperCase(sentence) {
   return transformedWords.join(" ");
 }
 
-console.log(sentenceToUpperCase("test"));
-console.log(sentenceToUpperCase("Ala ma kota"));
-console.log(sentenceToUpperCase("Raz dwa trzy cztery"));
+console.log(sentenceToUpperCase("test")); // TEST
+console.log(sentenceToUpperCase("Ala ma kota")); // Ala MA kota
+console.log(sentenceToUpperCase("Raz dwa trzy cztery")); // Raz DWA trzy CZTERY
